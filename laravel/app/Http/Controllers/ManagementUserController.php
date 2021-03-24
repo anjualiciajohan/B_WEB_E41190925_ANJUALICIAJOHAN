c92c02b3a227198f6f8ae0d7f7e8f5ffa2ce316a
anjualiciajohan <anjujohan0@gmail.com>
2021-03-24 12:24:12 +0700

    summary controller

---
+++ laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+class ManagementUseController extends Controller
+{
+    public function index(){
+        return "halo ini adalah method index, dalam controller ManagementUser.";
+    }
+}
