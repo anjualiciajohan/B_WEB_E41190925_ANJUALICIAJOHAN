89568272d84648db4f1c062755c738eb4b5615a6
anjualiciajohan <anjujohan0@gmail.com>
2021-03-26 11:51:17 +0700

    summary_management

--- laravel/app/Http/Controllers/ManagementUserController.php
+++ laravel/app/Http/Controllers/ManagementUserController.php
@@ -2,10 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ManagementUser;
 use Illuminate\Http\Request;
-class ManagementUseController extends Controller
+
+class ManagementUserController extends Controller
 {
-    public function index(){
-        return "halo ini adalah method index, dalam controller ManagementUser.";
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "Method ini nantinya akan digunakan untuk mengambil semua data user";
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return "Method ini nantinya akan digunakan untuk menciptakan data user yang baru";
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ManagementUser  $managementUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ManagementUser $managementUser)
+    {
+        return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id=" .$id;
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ManagementUser  $managementUser
+     * @return \Illuminate\Http\Response
+     */
+    public function edit( $id)
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" .$id;
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ManagementUser  $managementUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Method ini nantinya akan digunakan untuk mengubah form untuk mengubah data user dengan id=" .$id;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ManagementUser  $managementUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menghapus form data user dengan id=" .$id;
     }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ManagementUserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route ::('user','ManagementUserController@index');
+//Route::get('/user', [ManagementUserController::class,'index']);
+Route::resource('/user', ManagementUserController::class);
 Route::get('/', function () {
     return view('welcome');
 });
